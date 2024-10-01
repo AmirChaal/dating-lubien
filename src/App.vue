668eafac809d2773b3eef3632bcab644a2abd0d3
AmirCHaal (WSL) <mohamedamir.chaal@gmail.com>
2024-10-01 02:49:48 +0200

    add .env to control app behavior

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,8 @@ export default {
       return {
          // story: histoire as Story,
          // entryStepId: '0_0_0_0',
-         story: osuStory as Story,
-         entryStepId: 'XXXX_HUB_DORTOIR',
+         story: null as Story | null,
+         entryStepId: '',
          stepHistory: [] as Step[],
          storyVariablesHistory: [] as Record<string, boolean>[],
          currentCharacterImage: '',
@@ -91,13 +91,15 @@ export default {
          return areAllConditionsMet
       },
       async getCurrentCharacterImage() {
+         const charPath = import.meta.env.VITE_CHEMIN_DU_DOSSIER_PERSONNAGES
          if (this.currentStep == null || !this.currentStep.perso) return ''
-         const characterImage = import(`./assets/images/characters/${this.currentStep?.perso}`).then((img: any) => { return img.default })
+         const characterImage = import(`${charPath}${this.currentStep?.perso}`).then((img: any) => { return img.default })
          return characterImage
       },
       async getCurrentBackgroundImage() {
+         const backPath = import.meta.env.VITE_CHEMIN_DU_DOSSIER_FONDS
          if (this.currentStep == null || !this.currentStep.image) return ''
-         const backgroundImage = import(`./assets/images/backgrounds/${this.currentStep?.image}`).then((img: any) => { return img.default })
+         const backgroundImage = import(`${backPath}${this.currentStep?.image}`).then((img: any) => { return img.default })
          return backgroundImage
       },
       dumpEmptyArrays(array: Consequence[] | Condition[]): Consequence[] | Condition[] {
@@ -106,6 +108,8 @@ export default {
    },
 
    async mounted() {
+      this.story = await import(`${import.meta.env.VITE_CHEMIN_VERS_DOSSIER_JSON}${import.meta.env.VITE_NOM_DE_JSON_HISTOIRE}`).then(mod => { return mod.default })
+      this.entryStepId = import.meta.env.VITE_IDENTIFIANT_CHAPITRE_INITIAL
       this.stepHistory.push(getStepById(this.entryStepId, this.story))
       this.storyVariablesHistory = [this.getAllStoryVariables()]
    },
@@ -120,23 +124,25 @@ export default {
 </script>
 
 <template>
-   <DebugMenu :story="story" :entryStepId="entryStepId" :storyVariables="currentStoryVariables" class="h-full w-[30%]" />
-   <div class="flex flex-col justify-center w-[60em]">
-      <div class="w-full">
-         <div class="relative imagesContainer h-[30em] w-full flex justify-center items-end overflow-hidden rounded-t-[0.75em]">
-            <img class="z-[-1] absolute w-full opacity-60" :src="currentBackgroundImage" alt="">
-            <img class="h-[calc(100%-2em)]" :src="currentCharacterImage" alt="">
-         </div>
-         <p class="ml-[0.5em] opacity-50 absolute translate-y-[-1.7em]">{{ currentStep?.identifiant }}</p>
-         <div class="text-lg h-[6em] p-[0.5em] bg-zinc-900 overflow-auto" v-html="currentStep?.texte" />
-         <div class="border-t-[3px] border-solid border-zinc-700 h-[15em] bg-zinc-900 rounded-b-[0.75em] p-[0.5em] overflow-auto">
-            <TextButton v-if="previousStep" @click="returnToPreviousStep()" mainText="(Retour...)" :secondaryText="previousStep?.identifiant" :validity="true" :visibility="true" :currentStoryVariables="currentStoryVariables" />
-            <template v-for="opt in currentStep?.options">
-               <TextButton @click="takeOption(opt)" :mainText="`${opt.question}`" :secondaryText="opt.vers" :conditions="dumpEmptyArrays(opt?.condition)" :consequences="dumpEmptyArrays(opt?.consequence)" :validity="getOptionValidity(opt)" :visibility="getOptionVisibility(opt)" :currentStoryVariables="currentStoryVariables" />
-            </template>
+   <template v-if="story">
+      <DebugMenu :story="story" :entryStepId="entryStepId" :storyVariables="currentStoryVariables" class="h-full w-[30%]" />
+      <div class="flex flex-col justify-center w-[60em]">
+         <div class="w-full">
+            <div class="relative imagesContainer h-[30em] w-full flex justify-center items-end overflow-hidden rounded-t-[0.75em]">
+               <img class="z-[-1] absolute w-full opacity-60" :src="currentBackgroundImage" alt="">
+               <img class="h-[calc(100%-2em)]" :src="currentCharacterImage" alt="">
+            </div>
+            <p class="ml-[0.5em] opacity-50 absolute translate-y-[-1.7em]">{{ currentStep?.identifiant }}</p>
+            <div class="text-lg h-[6em] p-[0.5em] bg-zinc-900 overflow-auto" v-html="currentStep?.texte" />
+            <div class="border-t-[3px] border-solid border-zinc-700 h-[15em] bg-zinc-900 rounded-b-[0.75em] p-[0.5em] overflow-auto">
+               <TextButton v-if="previousStep" @click="returnToPreviousStep()" mainText="(Retour...)" :secondaryText="previousStep?.identifiant" :validity="true" :visibility="true" :currentStoryVariables="currentStoryVariables" />
+               <template v-for="opt in currentStep?.options">
+                  <TextButton @click="takeOption(opt)" :mainText="`${opt.question}`" :secondaryText="opt.vers" :conditions="dumpEmptyArrays(opt?.condition)" :consequences="dumpEmptyArrays(opt?.consequence)" :validity="getOptionValidity(opt)" :visibility="getOptionVisibility(opt)" :currentStoryVariables="currentStoryVariables" />
+               </template>
+            </div>
          </div>
       </div>
-   </div>
+   </template>
 </template>
 
 <style scoped lang="scss"></style>
